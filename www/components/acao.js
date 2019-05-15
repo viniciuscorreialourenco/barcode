414a98311bfd61461502f3420e57cd39b351fb0e
luizamaro11 <luiz.amaro90@gmail.com>
2019-05-15 10:23:30 +0900

    projeto finalizado

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,21 @@
 // This is a JavaScript file
 
+function scanBarcode() {
+     window.plugins.barcodeScanner.scan( function(result) {
+          if(result.text == "280720550" ){
+               window.location.replace("codigo1.html");
+          }
+          else if(result.text == "989895555"){
+               window.location.replace("codigo2.html");
+          }
+          else if(result.text == "85236987"){
+               window.location.replace("codigo3.html");
+          }
+          else if(result.text == "85369877444"){
+               window.location.replace("codigo4.html");
+          }
+          else{
+               alert("código de barra inválido: " + error);
+          }
+    });
+}
